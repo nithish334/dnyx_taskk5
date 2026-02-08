90a70c278b3e35b7c012b0bdef1688828b2f8771
nithish334 <kumnithish122@gmail.com>
2026-02-08 17:06:07 +0530

    Update Blogcard.js

--- Dynx-Task5-main/src/components/Blogcard.js
+++ Dynx-Task5-main/src/components/Blogcard.js
@@ -1,8 +1,8 @@
 export default function BlogCard({ blog }) {
   return (
-    <div className="bg-gray-100 p-4 rounded shadow">
+    <div className="border p-4 rounded shadow">
       <h2 className="text-xl font-bold">{blog.title}</h2>
-      <p className="mt-2">{blog.content}</p>
+      <p>{blog.content}</p>
     </div>
   );
 }
