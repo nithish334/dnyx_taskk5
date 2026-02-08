218e00bfc187967b57161037c92ffed1b99ccf80
nithish334 <kumnithish122@gmail.com>
2026-02-08 17:42:04 +0530

    Update Home.js

--- Dynx-Task5-main/src/pages/Home.js
+++ Dynx-Task5-main/src/pages/Home.js
@@ -2,19 +2,20 @@ import { useState } from "react";
 import AddBlog from "../components/AddBlog";
 import BlogCard from "../components/BlogCard";
 
-export default function Home() {
+const Home = () => {
   const [blogs, setBlogs] = useState([]);
 
   return (
-    <div className="p-6">
-      <AddBlog setBlogs={setBlogs} />
+    <div>
+      <AddBlog blogs={blogs} setBlogs={setBlogs} />
 
-      <div className="mt-4 space-y-4">
-        {blogs.map(blog => (
-          <BlogCard key={blog.id} blog={blog} />
-        ))}
-      </div>
+      {blogs.map((blog) => (
+        <BlogCard key={blog.id} blog={blog} />
+      ))}
     </div>
   );
-}
+};
+
+export default Home;
+
 
